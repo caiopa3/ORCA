ae8aa0e66215d443adc324a6f44e327d164dafe8
Yslan De Jesus <ysn15costa@gmail.com>
2025-10-24 10:30:11 -0300

    Ícone não arrumado

--- ORCA/obj/Debug/net8.0-windows/ORCA.AssemblyInfo.cs
+++ ORCA/obj/Debug/net8.0-windows/ORCA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ORCA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c910a780ae1c61bb23911ae90efe6f43a18cf11f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f67c8fde6016fa4bd81aeaea6a720e5e9854d46f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ORCA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ORCA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
